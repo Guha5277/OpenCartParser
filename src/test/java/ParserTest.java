46df08a0e87ad416515495d37d6e44cbbe075fed
Алексей <guha5277@yandex.ru>
2020-04-13 16:48:16 +0700

    Промежуточная часть реализации парсинга веб-страниц

--- src/test/java/ParserTest.java
+++ src/test/java/ParserTest.java
@@ -1,9 +1,10 @@
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+
+import java.util.ArrayList;
 
 public class ParserTest {
     static Parser parser;
@@ -16,20 +17,21 @@ public class ParserTest {
 
     @Test
     public void remotePageNotNullTest() {
-        Document retrievedDocument = parser.getPage();
+        Document retrievedDocument = parser.getFullPage();
         Assert.assertNotNull(retrievedDocument);
     }
 
     @Test
-    public void retrievedCategoriesTest(){
-        Elements retrievedElements = parser.getCategories();
+    public void receivedLiquidPartOfPage(){
+        Elements retrievedElements = parser.getCategoriesPage();
         int count = retrievedElements.size();
         Assert.assertNotNull(retrievedElements);
         Assert.assertEquals(9, count);
     }
 
     @Test
-    public void testOutput(){
-        parser.showCategories();
+    public void receivedCategoryData(){
+        ArrayList<Category> list = parser.getCategoryNames();
+        Assert.assertEquals(9, list.size());
     }
 }
