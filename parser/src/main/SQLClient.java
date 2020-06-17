696327e42c7e2b92f809a9f5ab4d873c426fa579
Guha5277 <guha5277@yandex.ru>
2020-06-17 12:13:22 +0700

    Оптимизация работы SQLClient - частичное избавление от хардкода (вынос в отдельные переменные) в строках запроса

--- parser/src/main/SQLClient.java
+++ parser/src/main/SQLClient.java
@@ -19,6 +19,13 @@ class SQLClient {
     private static Connection connection;
     private static Statement statement;
     private static Logger LOG;
+    private static final String PRODUCT_TABLE = "liquids";
+    private static final String WAREHOUSE_TABLE = "warehouse";
+    private static final String REMAINS_TABLE = "product_remains";
+    private static final String CATEGORIES_TABLE = "categories";
+    private static final String USERS_TABLE = "users";
+    private static final String INFO_TABLE = "info";
+    private static final String SETTINGS_TABLE = "settings";
 
     static {
         LOG = LogManager.getLogger();
@@ -55,7 +62,7 @@ class SQLClient {
     }
 
     synchronized static void insertCategory(String categoryName) {
-        String query = String.format("INSERT INTO categories (name) VALUES ('%s')", categoryName);
+        String query = String.format("INSERT INTO " + CATEGORIES_TABLE + " (name) VALUES ('%s')", categoryName);
         try {
             statement.execute(query);
         } catch (SQLException e) {
@@ -66,7 +73,7 @@ class SQLClient {
     synchronized static int getCategoryID(String categoryName) {
         int categoryID = 0;
 
-        String query = String.format("SELECT id FROM categories WHERE name='%s'", categoryName);
+        String query = String.format("SELECT id FROM " + CATEGORIES_TABLE + " WHERE name='%s'", categoryName);
         try {
             ResultSet set = statement.executeQuery(query);
             if (set.next()) {
@@ -86,7 +93,7 @@ class SQLClient {
         index = groupName.indexOf('\'');
         if (index != -1) groupName = returnValidString(groupName, index);
 
-        String query = String.format(Locale.CANADA, "INSERT INTO liquids(name, url, price, category, groupName, strength, volume) VALUES('%s', '%s', %d, %d, '%s', %.1f, %d)",
+        String query = String.format(Locale.CANADA, "INSERT INTO " + PRODUCT_TABLE + "(name, url, price, category, groupName, strength, volume) VALUES('%s', '%s', %d, %d, '%s', %.1f, %d)",
                 name, url, price, category, groupName, strength, volume);
         try {
             statement.execute(query);
@@ -106,7 +113,7 @@ class SQLClient {
     }
 
     synchronized static void insertStore(int region, String city, String address, String phone) {
-        String query = String.format("INSERT INTO warehouse(region, city, address, phone) VALUES(%d, '%s', '%s', '%s')",
+        String query = String.format("INSERT INTO " + WAREHOUSE_TABLE + "(region, city, address, phone) VALUES(%d, '%s', '%s', '%s')",
                 region, city, address, phone);
         try {
             statement.execute(query);
@@ -121,7 +128,7 @@ class SQLClient {
     }
 
     synchronized static boolean isProductAlreadyInDB(String url) {
-        String query = String.format("SELECT name from liquids WHERE url='%s'", url);
+        String query = String.format("SELECT name from " + PRODUCT_TABLE + " WHERE url='%s'", url);
         try {
             ResultSet set = statement.executeQuery(query);
             int size = 0;
@@ -137,7 +144,7 @@ class SQLClient {
     }
 
     synchronized static List<Product> getAllProducts() {
-        String query = "SELECT * from liquids";
+        String query = "SELECT * from " + PRODUCT_TABLE;
         try {
             ResultSet set = statement.executeQuery(query);
             if (set != null) {
@@ -231,7 +238,7 @@ class SQLClient {
     }
 
     synchronized static List<Warehouse> getAllWarehouses() {
-        String query = "SELECT * from warehouse";
+        String query = "SELECT * from " + WAREHOUSE_TABLE;
         try {
             ResultSet set = statement.executeQuery(query);
             List<Warehouse> warehousesList = new ArrayList<>();
@@ -255,7 +262,7 @@ class SQLClient {
     }
 
     synchronized static void updateProductName(int id, String name) {
-        String query = String.format("UPDATE liquids SET name = '%s' WHERE id = %d", name, id);
+        String query = String.format("UPDATE " + PRODUCT_TABLE + " SET name = '%s' WHERE id = %d", name, id);
         try {
             statement.execute(query);
         } catch (SQLException e) {
@@ -264,7 +271,7 @@ class SQLClient {
     }
 
     synchronized static void updateProductGroupName(int id, String name) {
-        String query = String.format("UPDATE liquids SET groupName = '%s' WHERE id = %d", name, id);
+        String query = String.format("UPDATE " + PRODUCT_TABLE + " SET groupName = '%s' WHERE id = %d", name, id);
         try {
             statement.execute(query);
         } catch (SQLException e) {
@@ -273,7 +280,7 @@ class SQLClient {
     }
 
     synchronized static void updateProductCategory(int id, int category) {
-        String query = String.format("UPDATE liquids SET category = %d WHERE id = %d", category, id);
+        String query = String.format("UPDATE " + PRODUCT_TABLE + " SET category = %d WHERE id = %d", category, id);
         try {
             statement.execute(query);
         } catch (SQLException e) {
@@ -282,7 +289,7 @@ class SQLClient {
     }
 
     synchronized static void updateProductPrice(int id, int price) {
-        String query = String.format("UPDATE liquids SET price = %d WHERE id = %d", price, id);
+        String query = String.format("UPDATE " + PRODUCT_TABLE + " SET price = %d WHERE id = %d", price, id);
         try {
             statement.execute(query);
         } catch (SQLException e) {
@@ -291,7 +298,7 @@ class SQLClient {
     }
 
     synchronized static void updateProductVolume(int id, int volume) {
-        String query = String.format(Locale.CANADA, "UPDATE liquids SET volume = %d WHERE id = %d", volume, id);
+        String query = String.format(Locale.CANADA, "UPDATE " + PRODUCT_TABLE + " SET volume = %d WHERE id = %d", volume, id);
         try {
             statement.execute(query);
         } catch (SQLException e) {
@@ -300,7 +307,7 @@ class SQLClient {
     }
 
     synchronized static void updateProductStrength(int id, double strength) {
-        String query = String.format(Locale.CANADA, "UPDATE liquids SET strength = %.1f WHERE id = %d", strength, id);
+        String query = String.format(Locale.CANADA, "UPDATE " + PRODUCT_TABLE + " SET strength = %.1f WHERE id = %d", strength, id);
         try {
             statement.execute(query);
         } catch (SQLException e) {
@@ -309,8 +316,8 @@ class SQLClient {
     }
 
     synchronized static void updateProductRemains(int warehouseId, int productId, int remains) {
-        String updateQuery = String.format("UPDATE product_remains SET remains = %d WHERE warehouse_id = %d AND product_id = %d ", remains, warehouseId, productId);
-        String insertQuery = String.format("INSERT INTO product_remains (warehouse_id, product_id, remains) VALUES (%d, %d, %d);", warehouseId, productId, remains);
+        String updateQuery = String.format("UPDATE " + REMAINS_TABLE + " SET remains = %d WHERE warehouse_id = %d AND product_id = %d ", remains, warehouseId, productId);
+        String insertQuery = String.format("INSERT INTO " + REMAINS_TABLE + " (warehouse_id, product_id, remains) VALUES (%d, %d, %d);", warehouseId, productId, remains);
         try {
             int count = statement.executeUpdate(updateQuery);
             if (count == 0) {
@@ -322,7 +329,7 @@ class SQLClient {
     }
 
     synchronized static String getNickname(String login, String password) {
-        String query = String.format("SELECT nickname FROM users WHERE username='%s' AND password='%s'", login, password);
+        String query = String.format("SELECT nickname FROM " + USERS_TABLE + " WHERE username='%s' AND password='%s'", login, password);
         try {
             ResultSet set = statement.executeQuery(query);
             if (set.isClosed()) return null;
@@ -334,7 +341,7 @@ class SQLClient {
     }
 
     synchronized static int getUserRole(String nickname) {
-        String query = String.format("SELECT role FROM users WHERE nickname='%s'", nickname);
+        String query = String.format("SELECT role FROM " + USERS_TABLE + " WHERE nickname='%s'", nickname);
         try {
             ResultSet set = statement.executeQuery(query);
             if (set.isClosed()) return 4;
@@ -346,7 +353,7 @@ class SQLClient {
     }
 
     static int getProductsCount() {
-        String query = "SELECT COUNT(*) FROM liquids";
+        String query = "SELECT COUNT(*) FROM " + PRODUCT_TABLE;
         try {
             ResultSet set = statement.executeQuery(query);
             if (set.isClosed()) return 0;
@@ -358,7 +365,7 @@ class SQLClient {
     }
 
     static int getWarehousesCount() {
-        String query = "SELECT COUNT(*) FROM warehouse";
+        String query = "SELECT COUNT(*) FROM " + WAREHOUSE_TABLE;
         try {
             ResultSet set = statement.executeQuery(query);
             if (set.isClosed()) return 0;
@@ -370,13 +377,13 @@ class SQLClient {
     }
 
     static LocalDate getProcessLastRun(String process) {
-        String query = String.format("SELECT last_run FROM info WHERE process='%s'", process);
+        String query = String.format("SELECT last_run FROM " + INFO_TABLE + " WHERE process='%s'", process);
         try {
             ResultSet set = statement.executeQuery(query);
             if (set.isClosed()) return null;
             SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
 
-            Date date = null;
+            Date date;
             try {
                 date = formatter.parse(set.getString(1));
             } catch (ParseException e) {
@@ -390,9 +397,8 @@ class SQLClient {
         }
     }
 
-    static LocalTime getProcessLaunchTime(String process){
-        String query = String.format("SELECT start_time FROM settings WHERE process='%s'", process);
-        //String query = "SELECT start_time FROM settings WHERE process='updater'";
+    static LocalTime getProcessLaunchTime(String process) {
+        String query = String.format("SELECT start_time FROM " + SETTINGS_TABLE + " WHERE process='%s'", process);
         try {
             ResultSet set = statement.executeQuery(query);
             if (set.isClosed()) return null;
@@ -412,8 +418,8 @@ class SQLClient {
         }
     }
 
-    static int getProcessDayInterval(String process){
-        String query = String.format("SELECT day_interval FROM settings WHERE process='%s'", process);
+    static int getProcessDayInterval(String process) {
+        String query = String.format("SELECT day_interval FROM " + SETTINGS_TABLE + " WHERE process='%s'", process);
         try {
             ResultSet set = statement.executeQuery(query);
             if (set.isClosed()) return 0;
@@ -424,8 +430,8 @@ class SQLClient {
         }
     }
 
-    static boolean getProcessAutoStartState(String process){
-        String query = String.format("SELECT enable FROM settings WHERE process='%s'", process);
+    static boolean getProcessAutoStartState(String process) {
+        String query = String.format("SELECT enable FROM " + SETTINGS_TABLE + " WHERE process='%s'", process);
         try {
             ResultSet set = statement.executeQuery(query);
             if (set.isClosed()) return false;
@@ -436,8 +442,8 @@ class SQLClient {
         }
     }
 
-    synchronized static void updateProcessAutoStartState(String process, boolean state){
-        String query = String.format("UPDATE settings SET enable='%s' WHERE process='%s'", state, process);
+    synchronized static void updateProcessAutoStartState(String process, boolean state) {
+        String query = String.format("UPDATE " + SETTINGS_TABLE + " SET enable='%s' WHERE process='%s'", state, process);
         try {
             statement.execute(query);
             commit();
@@ -446,8 +452,8 @@ class SQLClient {
         }
     }
 
-    synchronized static void updateProcessDayInterval(String process, int interval){
-        String query = String.format("UPDATE settings SET day_interval=%d WHERE process='%s'", interval, process);
+    synchronized static void updateProcessDayInterval(String process, int interval) {
+        String query = String.format("UPDATE " + SETTINGS_TABLE + " SET day_interval=%d WHERE process='%s'", interval, process);
         try {
             statement.execute(query);
             commit();
@@ -456,8 +462,8 @@ class SQLClient {
         }
     }
 
-    synchronized static void updateProcessStartTime(String process, LocalTime time){
-        String query = String.format("UPDATE settings SET start_time='%d:%d:00' WHERE process='%s'", time.getHour(), time.getMinute(), process);
+    synchronized static void updateProcessStartTime(String process, LocalTime time) {
+        String query = String.format("UPDATE " + SETTINGS_TABLE + " SET start_time='%d:%d:00' WHERE process='%s'", time.getHour(), time.getMinute(), process);
         try {
             statement.execute(query);
             commit();
@@ -469,8 +475,8 @@ class SQLClient {
 
     synchronized static void updateUpdaterLastRun(int pausedAt, int totalUpdated, int totalFails) {
         LocalDate date = LocalDate.now();
-        String query = String.format("UPDATE info SET last_run='%d-%d-%d', paused_at=%d, total_updated=%d, total_fails=%d WHERE process='updater'", date.getYear(), date.getMonthValue(), date.getDayOfMonth(), pausedAt, totalUpdated, totalFails);
-        String insertQuery = String.format("INSERT INTO info (process, last_runs, paused_at, total_updated, total_fails) VALUES ('updater', '%d-%d-%d', %d, %d, %d)", date.getYear(), date.getMonthValue(), date.getDayOfMonth(), pausedAt, totalUpdated, totalFails);
+        String query = String.format("UPDATE " + INFO_TABLE + " SET last_run='%d-%d-%d', paused_at=%d, total_updated=%d, total_fails=%d WHERE process='updater'", date.getYear(), date.getMonthValue(), date.getDayOfMonth(), pausedAt, totalUpdated, totalFails);
+        String insertQuery = String.format("INSERT INTO " + INFO_TABLE + " (process, last_runs, paused_at, total_updated, total_fails) VALUES ('updater', '%d-%d-%d', %d, %d, %d)", date.getYear(), date.getMonthValue(), date.getDayOfMonth(), pausedAt, totalUpdated, totalFails);
         try {
             int count = statement.executeUpdate(query);
             if (count == 0) {
@@ -483,7 +489,7 @@ class SQLClient {
     }
 
     synchronized static int getLastUpdatedProductPosition() {
-        String query = "select paused_at from info where process='updater'";
+        String query = "select paused_at from " + INFO_TABLE + " where process='updater'";
         try {
             ResultSet set = statement.executeQuery(query);
             if (set.isClosed()) return 0;
@@ -496,8 +502,8 @@ class SQLClient {
 
     synchronized static void updateResearcherLastRun(int totalFound) {
         LocalDate date = LocalDate.now();
-        String query = String.format("UPDATE info SET last_run='%d-%d-%d', total_updated=%d WHERE process='researcher'", date.getYear(), date.getMonthValue(), date.getDayOfMonth(), totalFound);
-        String insertQuery = String.format("INSERT INTO info (process, last_runs, total_updated) VALUES ('researcher', '%d-%d-%d', %d)", date.getYear(), date.getMonthValue(), date.getDayOfMonth(), totalFound);
+        String query = String.format("UPDATE " + INFO_TABLE + " SET last_run='%d-%d-%d', total_updated=%d WHERE process='researcher'", date.getYear(), date.getMonthValue(), date.getDayOfMonth(), totalFound);
+        String insertQuery = String.format("INSERT INTO " + INFO_TABLE + " (process, last_runs, total_updated) VALUES ('researcher', '%d-%d-%d', %d)", date.getYear(), date.getMonthValue(), date.getDayOfMonth(), totalFound);
         try {
             int count = statement.executeUpdate(query);
             if (count == 0) {
